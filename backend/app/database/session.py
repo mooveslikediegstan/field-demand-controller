4aa63ab80b91ba03f9f417e47745b5acc3624202
mooveslikediegstan <diego.stanzani1@hotmail.com>
2026-08-06 20:38:02 -0300

    feat: corrected error on createengerine function passed the url argument by name

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -16,7 +16,7 @@ DB_NAME     = os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(url = DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
